f20637c0b462922d7d75fe69cc909ec2d22affd1
Brian Obot <brianobot9@gmail.com>
2023-02-05 01:18:27 +0100

    Add Serializers to user app

---
+++ user/serializers.py
@@ -0,0 +1,61 @@
+from django.contrib.auth.password_validation import validate_password
+from django.db import transaction
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = ['id', 'username', 'first_name', 'last_name']
+
+
+class LoginSerializer(TokenObtainPairSerializer):
+	@classmethod
+	def get_token(cls, user):
+		token = super().get_token(user)
+		token['userId'] = user.id
+		return token
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password]
+    )
+    passwordTwo = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'image', 'username', 'password', 'passwordTwo'
+        )
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+            'username': {'required': True},
+            'password': {'required': True},
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['passwordTwo']:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."}
+                )
+        return attrs
+
+    @transaction.atomic
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
