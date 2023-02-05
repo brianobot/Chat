be46b2c5b5b3eacee1b223f75102784a92ef9db8
Brian Obot <brianobot9@gmail.com>
2023-02-05 01:09:07 +0100

    Setup Chat Models and Serializers

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class Message(models.Model):
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='sender', on_delete=models.CASCADE)
+    recipient = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='recipient', on_delete=models.CASCADE)
+    is_read = models.BooleanField(default=False)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "TO: {self.recipient} FROM: {self.sender}"
+
+    class Meta:
+        ordering = ('timestamp' ,)
