887de3f546c9c051380b18c6358827fd1fea4530
Brian Obot <brianobot9@gmail.com>
2023-02-08 11:44:20 +0100

    🍃 Refactor and Update code

--- config/asgi.py
+++ config/asgi.py
@@ -1,20 +1,10 @@
-# I am Keeping this version above for personal forensic studies
-# import os
-
-# from django.core.asgi import get_asgi_application
-# from channels.routing import get_default_application
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
-# application = get_asgi_application()
-
-
-# New Version
 import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+
 from django.core.asgi import get_asgi_application
+
 import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
